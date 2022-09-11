dc3887d635d4f2711c9dd4d60b342d23cf16129b
Sergio Sinuco <ssinuco@gmail.com>
2022-09-11 15:05:16 -0500

    Vanilla JS - integration by iframe

---
+++ js/data.js
@@ -0,0 +1,21 @@
+const data = {
+  tractors: {
+    eicher: {
+      label: 'Eicher Diesel 215/16',
+      img: 'https://mi-fr.org/img/eicher.svg',
+      recommendations: ['porsche', 'fendt']
+    },
+    fendt: {
+      label: 'Fendt F20 Dieselroß',
+      img: 'https://mi-fr.org/img/fendt.svg',
+      recommendations: ['porsche', 'eicher']
+    },
+    porsche: {
+      label: 'Porsche-Diesel Master 419',
+      img: 'https://mi-fr.org/img/porsche.svg',
+      recommendations: ['eicher', 'fendt']
+    }
+  }
+};
+
+export default data;
